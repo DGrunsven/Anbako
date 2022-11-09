2fb0de34a4f2a1da7aa75d239f7aba60b2dfc581
Donna van Grunsven <37537785+DGrunsven@users.noreply.github.com>
2022-11-09 10:23:00 +0100

    Spacing of menu items

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import logo from './anbako-mock-white.png';
 import { Button } from './Button';
