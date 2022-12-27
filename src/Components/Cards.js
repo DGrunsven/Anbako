290b444499239a2ebc6e21e9fbd7942ba085f887
DGrunsven <donna_vangrunsven@hotmail.com>
2022-12-27 15:02:38 +0100

    Over ons

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import './Cards.css';
-import photo from './man.jpg';
+import anne from './anne.jpg';
+import herman from './herman.jpg';
 
 function Cards() {
   return (   
       <div className='cards_container'>
         <div className='cards_wrapper'>
         <div className='cards'>
-      <h1 class="main_heading">Over ons</h1>
       <div className='text-wrapper'>
-      <p className='inhoud-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictumst quisque sagittis purus sit amet volutpat. Dapibus ultrices in iaculis nunc. Augue mauris augue neque gravida in fermentum. Tincidunt id aliquet risus feugiat in ante metus dictum at. Euismod nisi porta lorem mollis aliquam ut. Metus vulputate eu scelerisque felis. Elit at imperdiet dui accumsan sit amet nulla. Neque egestas congue quisque egestas. Vitae purus faucibus ornare suspendisse sed nisi. Sed vulputate mi sit amet mauris commodo quis imperdiet.
-</p>
+      <h1 class="main_heading">Over ons</h1>
+      <p>
+      ANBAKO Administratie is in 2005 gestart door Herman. Per 1 januari 2023 wordt het kantoor in een gezamenlijke vorm voortgezet met dochter Anne.
+      </p>
+      <br></br>
+      <p>Wij richten ons met name op het MKB en particulieren. Hierin staan persoonlijk contact en een laagdrempelige benadering centraal. Het uitgangspunt is om maatwerk te leveren door klantgericht te werken. Het is voor ons belangrijk om uw administratie naar uw wens in te richten en nemen daarbij graag de administratieve werkzaamheden uit handen. De mate waarin, bepaalt u zelf. 
+      Neem geheel vrijblijvend contact met ons op om de mogelijkheden voor u of uw onderneming te bespreken.
+      test</p>
+      <br></br>
+      <p>Neem geheel vrijblijvend contact met ons op om de mogelijkheden voor u of uw onderneming te bespreken.</p>
+      <br></br>
+      </div>
+      <div className='contact-container'>
+      <div className='contact-card-container'>
+      <img src={herman} alt="Herman" className='contact-foto'/>
+      <div className='contact-text'>
+      <p>Herman Janssen</p>
+      <p>06-46438073</p>
+      <p>herman@anbako.nl</p>
+      </div>
+      </div>
+      <div className='contact-card-container'>
+      <img src={anne} alt="Anne" className='contact-foto'/>
+      <div className='contact-text'>
+      <p>Anne van Dinther-Janssen</p>
+      <p>06-31907197</p>
+      <p>anne@anbako.nl</p>
+      </div>
+      </div>
+      </div>
 <div className='empty-block-text'></div>
-<img src={photo} alt="Anne en Herman" className='anne-herman'/>
-          </div>
+
+
         </div>
       </div>
     </div>
