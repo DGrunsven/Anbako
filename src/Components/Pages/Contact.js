dfe2331ef6599b71032b17e36e59e6adc5f2f54d
DGrunsven <donna_vangrunsven@hotmail.com>
2023-01-07 22:34:15 +0100

    Responsive edit excl navbar

--- src/Components/Pages/Contact.js
+++ src/Components/Pages/Contact.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import '../../App.css';
-import ContactForm3 from './../ContactForm3';
+import React from "react";
+import "./Contact.css";
+import ContactForm from "./../ContactForm";
 
 export default function Contact() {
-  return <><div className='contact'>
-      <div className='contact-form-container'>
-      <h1>Contact</h1>
-      </div> 
-      <p className='cf-text'>Neem voor meer informatie vrijblijvend contact met ons op via het onderstaande contact formulier.</p>   
-      <div className='cf-content'>
-        <ContactForm3 />
+  return (
+    <>
+      <div className="contact">
+        <div className="contact-form-container">
+          <h1>Contact</h1>
+        </div>
+        <p className="cf-text">
+          Neem voor meer informatie vrijblijvend contact met ons op via het
+          onderstaande contact formulier.
+        </p>
+        <div className="cf-content">
+          <ContactForm />
+        </div>
       </div>
-  </div></>;
+    </>
+  );
 }
--- src/Components/Pages/OverOns.js
+++ src/Components/Pages/OverOns.js
@@ -1,45 +1,71 @@
-import React from 'react';
-import '../../App.css';
-import anne from './../anne-2.jpg'; 
-import herman from './../herman-2.jpg';
+import React from "react";
+import "./OverOns.css";
+import anne from "./../anne-2.jpg";
+import herman from "./../herman-2.jpg";
 
 export default function OverOns() {
-  return <><div className='overOns'><div className='cards_container'>
-    <div className='cards_wrapper'>
-      <div className='cards'>
-        <div className='text-wrapper'>
-          <h1 className='main_heading'>Over ons</h1>
-          <p className='over-ons-text'>
-            ANBAKO Administratie is in 2005 gestart door Herman. Per 1 januari 2023 is het kantoor in een gezamenlijke vorm voortgezet met dochter Anne.
-          </p>
-          <br></br>
-          <p className='over-ons-text'>Wij richten ons met name op het MKB en particulieren. Hierin staan persoonlijk contact en een laagdrempelige benadering centraal. Het uitgangspunt is om maatwerk te leveren door klantgericht te werken. Het is voor ons belangrijk om uw administratie naar uw wens in te richten en nemen daarbij graag de administratieve werkzaamheden uit handen. De mate waarin, bepaalt u zelf.
-            Neem geheel vrijblijvend contact met ons op om de mogelijkheden voor u of uw onderneming te bespreken.
-          </p>
-          <br></br>
-          <p className='over-ons-text'>Neem geheel vrijblijvend contact met ons op om de mogelijkheden voor u of uw onderneming te bespreken.</p>
-          <br></br>
-        </div>
-        <div className='contact-container'>
-          <div className='contact-card-container'>
-            <img src={herman} alt="Herman" className='contact-foto' />
-            <div className='contact-text'>
-              <p className='contact-naam'>Herman Janssen</p>
-              <a className='ct' href="tel:0646438073">06 46438073</a>
-              <a className='ct' href="mailto: herman@anbako.nl">herman@anbako.nl</a>
-            </div>
-          </div>
-          <div className='contact-card-container'>
-            <img src={anne} alt="Anne" className='contact-foto' />
-            <div className='contact-text'>
-              <p className='contact-naam'>Anne van Dinther-Janssen</p>
-              <a className='ct' href="tel:0631907197">06 31907197</a>
-              <a className='ct' href="mailto: anne@anbako.nl">anne@anbako.nl</a>
+  return (
+    <>
+      <div className="overOns">
+        <div className="cards_container">
+          <div className="cards_wrapper">
+            <div className="cards">
+              <div className="text-wrapper">
+                <h1 className="main_heading">Over ons</h1>
+                <p className="over-ons-text">
+                  ANBAKO Administratie is in 2005 gestart door Herman. Per 1
+                  januari 2023 is het kantoor in een gezamenlijke vorm
+                  voortgezet met dochter Anne.
+                </p>
+                <br></br>
+                <p className="over-ons-text">
+                  Wij richten ons met name op het MKB en particulieren. Hierin
+                  staan persoonlijk contact en een laagdrempelige benadering
+                  centraal. Het uitgangspunt is om maatwerk te leveren door
+                  klantgericht te werken. Het is voor ons belangrijk om uw
+                  administratie naar uw wens in te richten en nemen daarbij
+                  graag de administratieve werkzaamheden uit handen. De mate
+                  waarin, bepaalt u zelf. Neem geheel vrijblijvend contact met
+                  ons op om de mogelijkheden voor u of uw onderneming te
+                  bespreken.
+                </p>
+                <br></br>
+                <p className="over-ons-text">
+                  Neem geheel vrijblijvend contact met ons op om de
+                  mogelijkheden voor u of uw onderneming te bespreken.
+                </p>
+                <br></br>
+              </div>
+              <div className="contact-container">
+                <div className="contact-card-container">
+                  <img src={herman} alt="Herman" className="contact-foto" />
+                  <div className="contact-text">
+                    <p className="contact-naam">Herman Janssen</p>
+                    <a className="ct" href="tel:0646438073">
+                      06 46438073
+                    </a>
+                    <a className="ct" href="mailto: herman@anbako.nl">
+                      herman@anbako.nl
+                    </a>
+                  </div>
+                </div>
+                <div className="contact-card-container">
+                  <img src={anne} alt="Anne" className="contact-foto" />
+                  <div className="contact-text">
+                    <p className="contact-naam">Anne van Dinther-Janssen</p>
+                    <a className="ct" href="tel:0631907197">
+                      06 31907197
+                    </a>
+                    <a className="ct" href="mailto: anne@anbako.nl">
+                      anne@anbako.nl
+                    </a>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-  </div></>;
+    </>
+  );
 }
